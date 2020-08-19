448198a2c4cbf1dcaa00ee50cf618fa51abbb089
Gordon Mackie <gordon.mackie@openfeature.co.uk>
2020-08-19 12:06:42 +0100

    Route updates

--- ChordDataAPI/Controllers/ChordDataController.cs
+++ ChordDataAPI/Controllers/ChordDataController.cs
@@ -10,7 +10,7 @@
     using Models;
 
     [ApiController]
-    [Route("[controller]")]
+    [Route("v1/api")]
     public class ChordDataController : ControllerBase
     {
         private readonly IChordData chordData;
@@ -27,6 +27,7 @@
         /// </summary>
         /// <returns>JSON structure</returns>
         [HttpGet]
+        [Route("chordData")]
         public ChordData ChordData()
         {
             return (ChordData)this.chordData;
